b30a3a59c603e1a48331b534cfdb55b4b38c53b9
Yunhao Gao <as510891328@gmail.com>
2020-08-05 03:30:47 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,8 +24,9 @@ end
 
 def return_string_array(array)
   counter = 0 
+  arr = []
   while array[counter]
-    puts array[counter].to_s
+    arr.push(array[counter].to_s)
     counter += 1
   end
 end
