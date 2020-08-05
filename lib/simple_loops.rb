4ad42facea614fd1011796d151357a894c06bd03
Yunhao Gao <as510891328@gmail.com>
2020-08-05 03:28:33 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,3 +1,31 @@
 # Write your methods here
 
-def
+def loop_message_five_times(message)
+  puts "message"
+  puts "message"
+  puts "message"
+  puts "message"
+  puts "message"
+end
+
+def loop_message_n_times(message, n)
+  n.times do
+    puts "message"
+  end
+end
+
+def output_array(array)
+  counter = 0
+  while array[counter]
+    puts array[counter] 
+    counter += 1   
+  end
+end
+
+def return_string_array(array)
+  counter = 0 
+  while array[counter]
+    put array[counter].to_s
+    counter += 1
+  end
+end
